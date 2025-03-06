93eb90b58326dd6976635b185ac93bf21e689d30
tball <taylorballcpt@gmail.com>
2025-03-06 11:07:24 -0600

    push to launch

--- src/components/DemoModeToggle.js
+++ src/components/DemoModeToggle.js
@@ -1,7 +1,10 @@
+// src/components/DemoModeToggle.js
 import React, { useState, useEffect } from "react";
 import { Button, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const DemoModeToggle = () => {
+  const theme = useTheme();
   const [demoMode, setDemoMode] = useState(
     localStorage.getItem("demoMode") === "true"
   );
@@ -15,8 +18,22 @@ const DemoModeToggle = () => {
   };
 
   return (
-    <Box sx={{ mb: 2 }}>
-      <Button variant="outlined" onClick={toggleDemoMode}>
+    <Box sx={{ m: 1 }}>
+      <Button
+        variant="outlined"
+        onClick={toggleDemoMode}
+        sx={{
+          color: theme.palette.mode === "light" ? "black" : "yellow",
+          borderColor: theme.palette.mode === "light" ? "black" : "yellow",
+          "&:hover": {
+            borderColor: theme.palette.mode === "light" ? "black" : "yellow",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "rgba(0,0,0,0.04)"
+                : "rgba(255,255,0,0.08)",
+          },
+        }}
+      >
         {demoMode ? "Disable Demo Mode" : "Enable Demo Mode"}
       </Button>
     </Box>
